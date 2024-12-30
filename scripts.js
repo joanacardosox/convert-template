02c58a5f463152e0a5493bf992fe7d6b4445a53d
Joana Maria <joanamariacardoso1601@gmail.com>
2024-12-30 20:10:32 -0300

    displaying the footer

--- scripts.js
+++ scripts.js
@@ -1,5 +1,49 @@
-const amount = document.getElementById("amount");
+// Cotação de moedas do dia
 
+const USD = 4.87;
+const EUR = 6.0;
+const GBP = 6.08;
+
+// Obtendo os elementos do formulário
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
+
+// Manipulando o input amount para receber somente números
 amount.addEventListener("input", () => {
-  console.log(amount.value);
+  const hasCharactersRegex = /\D+/g;
+  amount.value = amount.value.replace(hasCharactersRegex, "");
 });
+
+// Capturando o evento de submit (Enviar) no formulário.
+form.onsubmit = (event) => {
+  event.preventDefault(); // Corrigido o erro de digitação
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€");
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£");
+      break;
+    default:
+      console.log("Selecione uma moeda válida.");
+  }
+};
+
+// Função para converter a moeda
+function convertCurrency(amount, price, symbol) {
+  try {
+    //Aplica a classe que exibe o footer para mostrar o resultado
+    footer.classList.add("show-result");
+  } catch (error) {
+    // Remove a classe do footer removendo ele da tela
+    console.log(error);
+    footer.classList.remove("show-result");
+    alert("Não foi possivel!");
+  }
+}
