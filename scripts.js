a62ed6156d51184be5a1422a0ef3e7274aa0d8e4
Joana Maria <joanamariacardoso1601@gmail.com>
2025-01-01 13:19:57 -0300

    displaying the total value

--- scripts.js
+++ scripts.js
@@ -10,6 +10,7 @@ const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 const footer = document.querySelector("main footer");
 const description = document.getElementById("description");
+const result = document.getElementById("result");
 
 // Manipulando o input amount para receber somente números
 amount.addEventListener("input", () => {
@@ -39,6 +40,12 @@ function convertCurrency(amount, price, symbol) {
   try {
     //Exibindo de forma dinamica
     description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+
+    //Calcula o total
+    let total = amount * price;
+
+    //Exibir o resultado total
+    result.textContent = total;
 
     //Aplica a classe que exibe o footer para mostrar o resultado
     footer.classList.add("show-result");
